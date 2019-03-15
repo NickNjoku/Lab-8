ce59293511d9971823c17564a241d0ab8f1c940c
Nick Njoku <nnjoku245@ou.edu>
2019-03-15 16:17:24 -0500

    updated Todo

---
+++ Lab 8/Todo.java
@@ -0,0 +1,57 @@
+
+public class Todo {/*
+	Lab Section_________: 12
+	Last Modified_______: 2019-03-15
+	OU ID_______________: 113437448
+	OU EMAIL____________: nnjoku245@ou.edu
+
+	Tasks:
+	1. Create Color enum with the constants and fields
+	2. Finish constructor and getters for Color enum
+	3. Create Shape enum with constants
+	4. Make toString method for Shape enum
+	5. Create Location enum with constants
+	6. Create GamePieceAppearance class, make fields
+	7. Create constructor or GamePieceAppearance
+	8. Make getters for Color and Shape for GamePieceAppearance
+	9. Create GamePiece enum (make constants and private fields)
+	10. Make GamePiece constructor 
+	11. Make getters for Color and Shape in GamePiece enum
+	12. Create movesFirst method for GamePiece
+	13. Make toString method for GamePiece
+	
+
+	Estimated Time (minutes):
+	1. 3
+	2. 5
+	3. 2
+	4. 5
+	5. 2
+	6. 2
+	7. 3
+	8. 1
+	9. 5
+	10. 3
+	11. 1
+	12. 6
+	13. 2
+	
+	
+
+	Actual Time (minutes):
+	1. 5
+	2. 5
+	3. 2
+	4. 5
+	5. 1
+	6. 2
+	7. 3
+	8. 1
+	9. 30
+	10. 2
+	11. 1
+	12. 5
+	13. 2
+	
+
+*/}
