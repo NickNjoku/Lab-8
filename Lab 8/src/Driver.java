618050a41e2ad0ee6e2b3c807a58078835d919d6
Nick Njoku <nnjoku245@ou.edu>
2019-03-15 13:03:53 -0500

    First commit

---
+++ Lab 8/src/Driver.java
@@ -0,0 +1,16 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		
+		
+		GamePieceAppearance c = new GamePieceAppearance(Color.RED, Shape.RACECAR);
+		
+		
+		
+		System.out.println(c.toString());
+
+	}
+
+}
---
+++ Lab 8/src/Shape.java
@@ -0,0 +1,13 @@
+
+public enum Shape {
+	
+	THIMBLE, BOOT, RACECAR;
+	
+	
+	@Override 
+	public String toString()
+	{
+		return this.name();
+	}
+
+}
