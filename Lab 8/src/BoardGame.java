6fc13dd902bfec32ec02a2ec15ec529f9f7565e1
Nick Njoku <nnjoku245@ou.edu>
2019-03-15 15:57:57 -0500

    Finished addPlayer method for BoardGame

--- Lab 8/src/BoardGame.java
+++ Lab 8/src/BoardGame.java
@@ -1,4 +1,34 @@
+import java.util.LinkedHashMap;
 
 public class BoardGame {
+	
+	protected LinkedHashMap<String, GamePiece> playerPieces;
+	protected LinkedHashMap<String, Location> playerLocation;
+	
+	
+	public BoardGame()
+	{
+		
+	}
+	
+	public boolean addPlayer(String playerName, GamePiece gamePiece, Location initialLocation)
+	{
+		if(!(playerPieces.containsKey(playerName)))
+		{
+			playerPieces.put(playerName, gamePiece);
+			playerLocation.put(playerName, initialLocation);
+			
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+		
+		
+	}
+	
+	
 
 }
